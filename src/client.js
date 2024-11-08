2d618e8aaa8d5129c6e11055ff8d366157b51f88
Tri Nguyen <115333206+TriNguyen52@users.noreply.github.com>
2024-11-09 02:07:35 +0800

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 const URL = 'https://jjscuzhupalgzcetojkc.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impqc2N1emh1cGFsZ3pjZXRvamtjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMDY0MjY5OSwiZXhwIjoyMDQ2MjE4Njk5fQ.RiY5dLYxD7dLQ8gYMFrOJqPZmyxnxSzX_BWh7J4pczo';
+const API_KEY = '';
 
 export const supabase = createClient(URL, API_KEY);
